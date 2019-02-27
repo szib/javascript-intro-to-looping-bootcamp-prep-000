fe2a24de6e495955b6c985d5238848c78b97794b
Ivan Szebenszki <szib77@gmail.com>
2019-02-27 22:56:26 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -16,4 +16,13 @@ function whileLoop(number) {
 
 function doWhileLoop(number) {
   
+  function incrementVariable() {
+    i = i + 1;
+    return i;
+  }
+  
+  do {
+    console.log('I run once regardless.')
+  } while(incrementVariable() < num)
+  
 }
